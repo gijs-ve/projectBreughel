f5cf439987dbf243357468b7eb87002d9ea351d8
AdneCode <super-gijs@hotmail.com>
2023-01-18 19:11:15 +0100

    feat: isAdminSelector

--- server/migrations/20230118155656-create-users.js
+++ server/migrations/20230118155656-create-users.js
@@ -21,6 +21,10 @@ module.exports = {
                 allowNull: false,
                 type: Sequelize.STRING,
             },
+            isAdmin: {
+                allowNull: false,
+                type: Sequelize.BOOLEAN,
+            },
             createdAt: {
                 allowNull: false,
                 type: Sequelize.DATE,
--- server/models/users.js
+++ server/models/users.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
             name: DataTypes.STRING,
             password: DataTypes.STRING,
             mail: DataTypes.STRING,
+            isAdmin: DataTypes.BOOLEAN,
         },
         {
             sequelize,
