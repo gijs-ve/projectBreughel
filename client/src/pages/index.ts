52cdd2eb5b602aa33658d4dfa3052ef3927ca345
AdneCode <super-gijs@hotmail.com>
2023-01-21 16:05:04 +0100

    feat: inNavbar to distinguish from admin

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Admin } from './Admin';
 import { Home } from './Home';
 import { Paintings } from './Paintings';
 import { Offerte } from './Offerte';
@@ -9,6 +10,7 @@ export type Page = {
     component: FC;
     icon: string;
     navText: string;
+    inNavarbar: boolean;
     path: string;
 };
 const pages: Page[] = [
@@ -16,31 +18,43 @@ const pages: Page[] = [
         component: Home,
         icon: 'none',
         navText: 'Home',
+        inNavarbar: true,
         path: '/',
     },
     {
         component: Paintings,
         icon: 'none',
         navText: 'Collecties',
+        inNavarbar: true,
         path: '/collecties',
     },
     {
         component: Offerte,
         icon: 'none',
         navText: 'Persoonlijk schilderij',
+        inNavarbar: true,
         path: '/offerte',
     },
     {
         component: Contact,
         icon: 'none',
         navText: 'Contact',
+        inNavarbar: true,
         path: '/contact',
     },
     {
         component: Winkelmand,
         icon: 'none',
         navText: 'Winkelmand',
+        inNavarbar: true,
         path: '/winkelmand',
+    },
+    {
+        component: Admin,
+        icon: 'none',
+        navText: 'Admin Paneel',
+        inNavarbar: false,
+        path: '/boekelo-zoo',
     },
 ];
 export { pages };
