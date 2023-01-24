3f044883f7b9bd12ae20a723cac7872be51a4c25
AdneCode <super-gijs@hotmail.com>
2023-01-25 00:27:29 +0100

    fix: migration order

--- server/migrations/20230118160216-create-paintings.js
+++ server/migrations/20230118160216-create-paintings.js
@@ -18,7 +18,7 @@ module.exports = {
                 type: Sequelize.INTEGER,
                 references: {
                     key: 'id',
-                    model: 'painter',
+                    model: 'painters',
                 },
                 onDelete: 'CASCADE',
                 onUpdate: 'CASCADE',
