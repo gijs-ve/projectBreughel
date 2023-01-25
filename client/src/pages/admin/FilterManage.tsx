eed39bccb194bb355f499844b505d7c4ab250682
AdneCode <super-gijs@hotmail.com>
2023-01-25 18:14:06 +0100

    fileManager

--- client/src/pages/admin/FilterManage.tsx
+++ client/src/pages/admin/FilterManage.tsx
@@ -2,10 +2,18 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { apiUrl } from '../../config/constants';
 import { Data } from '../../../../types/types';
+import { getFilters } from '../../utility/functions';
 
 const FilterManage = () => {
     const [filters, setFilters] = useState<[] | string[]>([]);
-    useEffect(() => {}, []);
-
+    useEffect(() => {
+        const data = getFilters();
+        setFilters(data);
+    }, []);
+    const CurrentFilters = filters.map((i: string) => {
+        <>
+            <div></div>
+        </>;
+    });
     return <></>;
 };
