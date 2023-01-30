fa8878fc57a64a1d0f5f010c3db1c92d960daaf5
AdneCode <super-gijs@hotmail.com>
2023-01-30 21:18:59 +0100

    fix: params to string

--- client/src/components/PaintingRow.tsx
+++ client/src/components/PaintingRow.tsx
@@ -1,24 +1,32 @@
 import { Props } from '../../../types/types';
+import { Link } from 'react-router-dom';
+
+const Default = () => {
+  return ( <tr>
+    <td className="pl-4">Error</td>
+    <td>Error</td>
+    <td>Error</td>
+    <td>Wijzig</td>
+    <td>Verwijder</td>
+  </tr>)
+}
 
 export const PaintingRow = (p: Props) => {
     const { painting } = p;
     if (!painting)
         return (
-            <tr>
-                <td className="pl-4">Error</td>
-                <td>Error</td>
-                <td>Error</td>
-                <td>Wijzig</td>
-                <td>Verwijder</td>
-            </tr>
+           <Default />
         );
     const { id, name, painterId } = painting;
+    if (!id || !name || !painterId) return (
+      <Default />
+   );
     return (
         <tr>
             <td className="pl-12">{id}</td>
             <td className="pl-12">{name}</td>
             <td className="pl-12">{painterId}</td>
-            <td className="pl-8">Wijzig</td>
+            <Link key={id} to={id.toString()}><td className="pl-8">Wijzig</td></Link>
             <td className="pl-8">Verwijder</td>
         </tr>
     );
