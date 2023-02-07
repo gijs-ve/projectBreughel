c7a8eef010f2c30d586473f698896f32d3b7111a
AdneCode <super-gijs@hotmail.com>
2023-02-07 15:18:08 +0100

    feat: admin slide page

--- client/src/pages/admin/AdminFrontpage.tsx
+++ client/src/pages/admin/AdminFrontpage.tsx
@@ -1,6 +1,7 @@
 import { getFavorites } from '../../utility/functions';
-import { Painting } from '../../../../types/types';
+import { Painting, Props } from '../../../../types/types';
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const AdminFrontpage = () => {
     const [favorites, setFavorites] = useState<Painting[] | null>(null);
@@ -12,5 +13,29 @@ export const AdminFrontpage = () => {
         };
         fetchFavorites();
     }, []);
-    return <>Schilderijen zichtbaar in slider</>;
+
+    return (
+        <>
+            <div>Schilderijen in slider</div>
+            <FavoriteField />
+        </>
+    );
+};
+
+const FavoriteField = (p: Props) => {
+    const navigate = useNavigate();
+    const { paintings } = p;
+    if (!paintings) return <></>;
+    const MappedPaintings = paintings.map((i: Painting) => {
+        if (!i.id) return <></>;
+        return (
+            <>
+                <div onClick={() => navigate(`${i.id ? i.id.toString() : ''}`)}>
+                    {i.name}
+                </div>
+                <div>Verwijder uit</div>
+            </>
+        );
+    });
+    return <>{MappedPaintings}</>;
 };
