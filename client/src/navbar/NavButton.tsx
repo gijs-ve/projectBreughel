33199d3c3fc8cdea6aade99e877235dd641fac1f
AdneCode <super-gijs@hotmail.com>
2023-02-26 20:40:35 +0100

    fix: hover css

--- client/src/navbar/NavButton.tsx
+++ client/src/navbar/NavButton.tsx
@@ -4,7 +4,11 @@ export const NavButton = (p: { text: string; hasToken: boolean }) => {
     console.log(color, text);
     return (
         <div
-            className={`text-2xl antialiased border-4 rounded-xl border-yellow-400 max-w-48 px-4 py-2 bg-${color}-200 text-gray-800 hover:bg-${color}-700`}
+            className={`text-2xl antialiase border-4 rounded-xl border-yellow-400 max-w-48 px-4 py-2 ${
+                hasToken
+                    ? 'bg-red-600 hover:bg-red-400 hover:border-red-400'
+                    : 'bg-yellow-300 hover:bg-yellow-500 hover:border-yellow-400'
+            } text-gray-800 ${hasToken ? '' : ''}`}
         >
             {text}
         </div>
